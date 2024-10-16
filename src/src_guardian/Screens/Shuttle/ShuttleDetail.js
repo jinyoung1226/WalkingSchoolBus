21dd15b85a4bca21c833e60a6ee66ce3be9961f9
suhwan kim <playground99@naver.com>
2024-10-17 01:04:08 +0900

    feat:운행 기능 업데이트 경유지 출석 완료시 이벤트 핸들러 구현 (인솔자중 한명이 출석 확인 완료시 모달 팝업, 출석 완료 상태 업데이트, 쿼리 리페치) 인솔자 출근/퇴근 기능 구현 업데이트 필요한 기능: 1. 출근하기 이후 마지막 경유지 출석 완료시 퇴근하기 버튼 활성화, 완료 전까지는 운행중이라는 비활성화 버튼 제공 2. 출근 여부에 따라 개별 학생 출석 상태 및 경유지 출석 완료 기능 제한

--- src/src_guardian/Screens/Shuttle/ShuttleDetail.js
+++ src/src_guardian/Screens/Shuttle/ShuttleDetail.js
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import { Text, TouchableOpacity, View} from 'react-native';
+import { Alert, Text, TouchableOpacity, View} from 'react-native';
 import {colors, textStyles} from '../../../styles/globalStyle';
 import {formatDate} from '../../../utils/formatDate';
 import {FlatList} from 'react-native-gesture-handler';
@@ -7,30 +7,70 @@ import WaypointCard from '../../../components/WaypointCard';
 import SchoolTimeComponent from '../../../components/SchoolTimeComponent';
 import CustomButton from '../../../components/CustomButton';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { getGroupForGuardian, getWaypoints } from '../../../api/shuttleApi';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { getGroupForGuardian, getWaypoints, startGuide, stopGuide } from '../../../api/shuttleApi';
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 import CustomHeader from '../../../components/CustomHeader';
 import useWebsocketStore from '../../../store/websocketStore';
 import MapIcon from '../../../assets/icons/MapIcon.svg';
+import eventEmitter from '../../../utils/eventEmitter';
+
 const ShuttleDetail = ({navigation}) => {
   const [isBeforeSchool, setIsBeforeSchool] = useState(true);
-
+  const [isGuideActive, setIsGuideActive] = useState(false);
   const {subscribeToChannel, unsubscribeToChannel} = useWebsocketStore();
   // today 날짜
   const formattedDate = formatDate();
   const insets = useSafeAreaInsets();
   const queryClient = useQueryClient();
 
+  // 인솔자가 배정된 그룹 정보 불러오기
   const { data: groupInfo, isPending: groupInfoIsPending, error: groupInfoError } = useQuery({
     queryKey: ['groupInfo'], 
     queryFn: getGroupForGuardian
   });
-  // 인솔자가 배정된 그룹 정보 불러오기
+
+  // 각 경유지 정보 불러오기
   const { data: waypoints, isPending: waypointsIsPending, error: waypointsError } = useQuery({
     queryKey: ['waypoints'], 
     queryFn: getWaypoints
   });
 
+  // 인솔자 출근/퇴근 API 호출
+  const onGuideButtonPress = () => {
+    const useMutateGuideActive = useMutation({
+      mutationFn: () => startGuide(groupInfo.id),
+      onSuccess: (response) => {
+        console.log(response);
+        setIsGuideActive(response.isGuideActive);
+      },
+      onError: (error) => {
+        console.log(error);
+      }
+    });
+  
+    const useMutateGuideDeactive = useMutation({
+      mutationFn: () => stopGuide(groupInfo.id),
+      onSuccess: (response) => {
+        console.log(response);
+        setIsGuideActive(response.isGuideActive);
+      },
+      onError: (error) => {
+        console.log(error);
+      }
+    });
+
+    if (isGuideActive) {
+      useMutateGuideDeactive.mutate();
+      return;
+    }
+
+    if (!isGuideActive) {
+      useMutateGuideActive.mutate();
+      return;
+    }
+  }
+
+  // WebSocket 구독
   useEffect(() => {
     if (groupInfo) {
       subscribeToChannel({
@@ -38,28 +78,36 @@ const ShuttleDetail = ({navigation}) => {
         callback: message => {
           const newMessage = JSON.parse(message.body);
           console.log(newMessage);
-          const { studentId, attendanceStatus, waypointId } = newMessage;
-          queryClient.setQueryData(['waypoints'], (oldData) => {
-            if (!oldData) return;
-            return oldData.map((waypoint) => {
-              if (waypoint.waypointId === waypointId) {
-                console.log('Updating waypoint:', waypoint.waypointName);
-                return { ...waypoint, currentCount: attendanceStatus === 'PRESENT' ? waypoint.currentCount + 1 : waypoint.currentCount - 1 };
-              }
-              return waypoint;
+          // 학생 개별 출석 상태 변경시 캐시 업데이트
+          if (newMessage.messageType === 'ATTENDANCE_CHANGE') {
+            const { studentId, attendanceStatus, waypointId } = newMessage;
+            // 경유지 정보에 출석 수 업데이트
+            queryClient.setQueryData(['waypoints'], (oldData) => {
+              if (!oldData) return;
+              return oldData.map((waypoint) => {
+                if (waypoint.waypointId === waypointId) {
+                  console.log('Updating waypoint:', waypoint.waypointName);
+                  return { ...waypoint, currentCount: attendanceStatus === 'PRESENT' ? waypoint.currentCount + 1 : waypoint.currentCount - 1 };
+                }
+                return waypoint;
+              });
             });
-          });
-          // React Query 캐시 업데이트
-          queryClient.setQueryData(['studentsInfo', waypointId], (oldData) => {
-            if (!oldData) return;
-            return oldData.map((student) => {
-              if (student.studentId === studentId) {
-                console.log('Updating student:', student.name);
-                return { ...student, attendanceStatus };
-              }
-              return student;
+            // 학생 정보에 출석 상태 업데이트
+            queryClient.setQueryData(['studentsInfo', waypointId], (oldData) => {
+              if (!oldData) return;
+              return oldData.map((student) => {
+                if (student.studentId === studentId) {
+                  console.log('Updating student:', student.name);
+                  return { ...student, attendanceStatus };
+                }
+                return student;
+              });
             });
-          });
+          }
+          // 경유지 출석 확인 완료시 이벤트 발생 (shuttleStudentsList.js에서 이벤트 처리)
+          if (newMessage.messageType === 'ATTENDANCE_COMPLETE') {
+            eventEmitter.emit('attendanceComplete', newMessage);
+          }
         }
       });
     }
@@ -110,7 +158,9 @@ const ShuttleDetail = ({navigation}) => {
               subtitle={`출석 ${item.currentCount}/${item.studentCount}`}
               onPress={() =>
                 navigation.navigate('ShuttleStudentsList', {
-                  waypointId: item.waypointId, waypointName: item.waypointName, groupInfo: groupInfo
+                  waypoint: item, 
+                  groupInfo: groupInfo, 
+                  previousAttendanceComplete: waypoints[item.waypointOrder - 2] ? waypoints[item.waypointOrder - 2].attendanceComplete : true
                 })
               }
               isFirstItem={item.waypointOrder === 1}
@@ -120,7 +170,7 @@ const ShuttleDetail = ({navigation}) => {
         }}
       />
       <View style={{padding:16}}>
-        <CustomButton title={'출근하기'} onPress={() => {}}/>
+        <CustomButton title={isGuideActive ? '퇴근하기' : '출근하기'} onPress={() => {onGuideButtonPress()}}/>
       </View>
     </View>
   );
--- src/src_guardian/Screens/Shuttle/ShuttleStudentsList.js
+++ src/src_guardian/Screens/Shuttle/ShuttleStudentsList.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import {View, Text, Alert} from 'react-native';
+import React, {useState, useEffect, useMemo} from 'react';
+import {View, Text} from 'react-native';
 import {colors, textStyles} from '../../../styles/globalStyle';
-import {authApi} from '../../../api/api';
 import { FlatList } from 'react-native-gesture-handler';
 import StudentCard from '../../../components/StudentCard';
 import ConfirmModal from '../../../components/ConfirmModal';
@@ -12,40 +11,66 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import CustomButton from '../../../components/CustomButton';
 import useWebsocketStore from '../../../store/websocketStore';
 import MapIcon from '../../../assets/icons/MapIcon.svg';
+import SingleActionModal from '../../../components/SingleActionModal';
+import CheckIcon from '../../../assets/icons/CheckIcon.svg';
+import ThinkingFaceIcon from '../../../assets/icons/ThinkingFaceIcon.svg';
+import HuggingFaceIcon from '../../../assets/icons/HuggingFaceIcon.svg';
+import eventEmitter from '../../../utils/eventEmitter';
 
 const ShuttleStudentsList = ({navigation, route}) => {
-  const {waypointId, waypointName, groupInfo} = route.params;
-  // const [studentsInfo, setStudentInfo] = useState([]);
-  const [modalVisible, setModalVisible] = useState(false);
+  const {waypoint, groupInfo, previousAttendanceComplete} = route.params;
+  const [attendanceModalVisible, setAttendanceModalVisible] = useState(false);
+  const [cancelModalVisible, setCancelModalVisible] = useState(false);
+  const [completeModalVisible, setCompleteModalVisible] = useState(false);
+  const [nonCompleteModalVisible, setNonCompleteModalVisible] = useState(false);
+  const [completeMessage, setCompleteMessage] = useState(null);
   const [selectedStudentId, setSelectedStudentId] = useState(null);
+  const [isAttendanceComplete, setIsAttendanceComplete] = useState(waypoint.attendanceComplete);
   const insets = useSafeAreaInsets();
   const { publish } = useWebsocketStore();
   const queryClient = useQueryClient();
 
+  useEffect(() => {
+    // 경유지 출석 완료 이벤트 핸들러 (인솔자중 한명이 출석 확인 완료시 모달 팝업, 출석 완료 상태 업데이트, 쿼리 리페치)
+    const attendanceCompleteHandler = (message) => {
+      if (message.waypointId == waypoint.waypointId) {
+        setCompleteMessage(message);
+        setCompleteModalVisible(true);
+        setIsAttendanceComplete(true);
+        queryClient.invalidateQueries({ queryKey: ['studentsInfo', waypoint.waypointId] });
+        queryClient.invalidateQueries({ queryKey: ['waypoints'] });
+      }
+    };
+    eventEmitter.on('attendanceComplete', attendanceCompleteHandler);
+    return () => {
+      eventEmitter.removeListener('attendanceComplete', attendanceCompleteHandler);
+    }
+  }, []);
+
   // 각 경유지에 배정된 학생 불러오기
   const { data: studentsInfo, isPending: studentsInfoIsPending, error: studentsInfoError } = useQuery({
-    queryKey: ['studentsInfo', waypointId], 
-    queryFn: () => getStudentsByWaypoint(waypointId)
+    queryKey: ['studentsInfo', waypoint.waypointId], 
+    queryFn: () => getStudentsByWaypoint(waypoint.waypointId)
   });
 
-  const mutaion = useMutation({
-    mutationFn: () => completeAttendance(waypointId),
-    onSuccess: (response) => {
-      Alert.alert('출석 완료', response.message);
-      // navigation.goBack();
-      queryClient.invalidateQueries(['studentsInfo', waypointId]);
-      queryClient.invalidateQueries(['waypoints']);
+  // 경유지별 출석 완료 API 호출
+  const useMutateAttendanceComplete = useMutation({
+    mutationFn: () => completeAttendance(waypoint.waypointId),
+    onError: (error) => {
+      console.log(error);
     }
   })
 
+  // 추후 스켈레톤 UI 추가
   if (studentsInfoIsPending) {
     return <View />;
   }
 
+  // 학생 개별 출석 상태 변경
   const onAttendanceButtonPress = async ({studentId, status}) => {
     if (status === 'PRESENT') {
       setSelectedStudentId(studentId);
-      setModalVisible(true);
+      setCancelModalVisible(true);
     }
     if (status === 'UNCONFIRMED') {
       console.log(studentId, status);
@@ -58,13 +83,104 @@ const ShuttleStudentsList = ({navigation, route}) => {
     }
   }
 
+  // 미인증 상태인 학생 목록
+  const absentStudents = studentsInfo.filter(student => student.attendanceStatus === 'UNCONFIRMED');
+
   return (
     <View style={{backgroundColor: 'white', flex: 1, paddingBottom: insets.bottom, paddingTop: insets.top}}>
+      <SingleActionModal
+        modalVisible={nonCompleteModalVisible}
+        setModalVisible={setNonCompleteModalVisible}
+        icon={<ThinkingFaceIcon />}
+        title={'이전 경유지의 출석이\n완료되지 않았어요'}
+        subtitle={'이전 경유지의 출석을 먼저 완료해주세요'}
+        confirmTitle={'돌아가기'}
+        onConfirm={() => {
+          setNonCompleteModalVisible(false);
+          navigation.goBack();
+        }}
+        isBackgroundclosable={false}
+      />
+      <SingleActionModal
+        modalVisible={completeModalVisible}
+        setModalVisible={setCompleteModalVisible}
+        icon={<HuggingFaceIcon />}
+        title={
+        <Text>
+          {`${completeMessage?.guardianName} 지도사님이 \n`}
+          <Text style={{color: colors.Main_Green}}>
+          {completeMessage?.waypointName}
+          </Text> 
+          {` 경유지의\n출석을 완료했어요!`}
+        </Text>}
+        // subtitle={'출석 확인이 완료되었습니다.'}
+        confirmTitle={'확인'}
+        onConfirm={() => {
+          setCompleteModalVisible(false);
+          navigation.goBack();
+        }}
+        isBackgroundclosable={false}
+      />
       <ConfirmModal
-        modalVisible={modalVisible}
-        setModalVisible={setModalVisible}
+        modalVisible={attendanceModalVisible}
+        setModalVisible={setAttendanceModalVisible}
+        title={'출결을 모두 확인하셨나요?'}
+        subtitle={
+          absentStudents.length > 0 ?
+          <View>
+            <Text
+              style={[
+                textStyles.R1,
+                {color: colors.Gray06, textAlign: 'center'},
+              ]}>
+              {'현재 미인증 상태인 '}
+              {absentStudents.map((student, index) => (
+                <Text 
+                  style={[textStyles.SB2, {color: colors.Main_Green}]}
+                  key={index}
+                >
+                  {student.name}{index === absentStudents.length - 1 ? '' : ', '}
+                </Text>
+              ))}
+              {' 학생은\n자동으로 결석 처리 됩니다.'}
+            </Text>
+            <View style={{height: 8}} />
+            <Text
+              style={[textStyles.R2, {color: colors.Red, textAlign: 'center'}]}
+            >
+              {'출결확인을 완료하면 출결을 다시 변경할 수 없습니다.'}
+            </Text>
+          </View> 
+          :
+          <Text
+          style={[textStyles.R1, {color: colors.Red, textAlign: 'center'}]}
+          >
+            {'출결확인을 완료하면\n출결을 다시 변경할 수 없습니다.'}
+          </Text>
+        }
+        cancelTitle={'아니오'}
+        confirmTitle={'네'}
+        onConfirm={() => {
+          useMutateAttendanceComplete.mutate();
+          setAttendanceModalVisible(false);
+        }}
+        onCancel={() => {
+          setAttendanceModalVisible(false);
+        }}
+        />
+      <ConfirmModal
+        modalVisible={cancelModalVisible}
+        setModalVisible={setCancelModalVisible}
         title={'출석을 취소하시겠어요?'}
-        subtitle={'출석을 취소하면 미인증 상태로 변경됩니다'}
+        subtitle={
+          <Text
+          style={[
+            textStyles.R1,
+            {color: colors.Gray06, textAlign: 'center'},
+          ]}>
+            {'출석을 취소하면 미인증 상태로 변경됩니다'}
+          </Text>
+        }
         cancelTitle={'아니요'}
         confirmTitle={'네, 취소할래요'}
         onConfirm={() => {
@@ -74,14 +190,14 @@ const ShuttleStudentsList = ({navigation, route}) => {
             studentId: selectedStudentId,
             attendanceStatus: 'UNCONFIRMED',
           });
-          setModalVisible(false);
+          setCancelModalVisible(false);
         }}
         onCancel={() => {
-          setModalVisible(false);
+          setCancelModalVisible(false);
         }}
       />
       <CustomHeader 
-        title={waypointName} 
+        title={waypoint.waypointName} 
         subtitle={groupInfo.groupName}
         subtitleVisible={true} 
         headerRight={<MapIcon/>} 
@@ -90,7 +206,7 @@ const ShuttleStudentsList = ({navigation, route}) => {
       <View style={{height: 16}} />
       <View style={{paddingHorizontal:32}}>
         <Text style={[textStyles.M2, {color: colors.Black}]}>
-          {`📌 현재 출결`}
+          {`📌 현재 출석`}
           <Text style={[textStyles.SB2, {color: colors.Red}]}>
             {` ${studentsInfo.filter((student) => student.attendanceStatus === 'PRESENT').length}/${studentsInfo.length}`}
           </Text>
@@ -103,6 +219,7 @@ const ShuttleStudentsList = ({navigation, route}) => {
         keyExtractor={(item) => item.studentId}
         renderItem={({item}) => (
           <StudentCard
+            changeStatusDisabled={isAttendanceComplete}
             initialStatus={item.attendanceStatus}
             studentId={item.studentId}
             name={item.name}
@@ -117,7 +234,17 @@ const ShuttleStudentsList = ({navigation, route}) => {
         ItemSeparatorComponent={() => <View style={{height: 16}} />}
       />
       <View style={{padding:16}}>
-        <CustomButton title={'출석완료'} onPress={() => {mutaion.mutate()}}/>
+        <CustomButton 
+          title={!isAttendanceComplete ? '출석 확인' : '출석 완료'}
+          onPress={() => {
+            if (!previousAttendanceComplete) {
+              setNonCompleteModalVisible(true);
+            } else {
+              setAttendanceModalVisible(true);
+            }
+          }}
+          disabled={isAttendanceComplete}
+        />
       </View>
     </View>
   );
