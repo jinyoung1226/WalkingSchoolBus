f808df7cd80c0269753ad6c61cde5cdb5d9b057c
suhwan kim <playground99@naver.com>
2024-11-12 02:54:06 +0900

    fix: 학부모 버전 인솔자 위치정보 재렌더링 오류 해결

--- src/src_parent/Screens/Shuttle/ShuttleMain.js
+++ src/src_parent/Screens/Shuttle/ShuttleMain.js
@@ -1,18 +1,24 @@
 import { useEffect } from 'react';
 import {Button, Text, View} from 'react-native';
-
+import useParentGroupInfo from '../../hooks/queries/useParentGroupInfo';
+import useWaypoints from '../../../src_guardian/hooks/queries/useWaypoints';
 const ShuttleMain = ({navigation}) => {
 
+  const { data: groupInfo, isPending: groupInfoIsPending, isSuccess: groupInfoIsSuccess } = useParentGroupInfo();
+  const { data: waypoints, isPending: waypointsIsPending, isSuccess: waypointsIsSuccess } = useWaypoints();
   useEffect(() => {
-    navigation.navigate('ShuttleDetail');
-  }, []);
+    if (groupInfo) {
+      navigation.navigate('ShuttleDetail', {groupInfo, waypoints});
+    }
+    
+  }, [groupInfo]);
 
   return (
     <View>
       <Text>Shuttle Main</Text>
       <Button title="운행 화면으로 돌아가기"
         onPress={() => {
-          navigation.navigate('ShuttleDetail');
+          navigation.navigate('ShuttleDetail', {groupInfo, waypoints});
         }}
       />
     </View>
